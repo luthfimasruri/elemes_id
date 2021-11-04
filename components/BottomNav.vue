218b52d2b536e7df799009736b153c2e809b78c0
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-11-04 12:29:25 +0700

    refactor: :recycle: refactor footer & add bottom nav

---
+++ components/BottomNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-bottom-navigation
+    v-if="$vuetify.breakpoint.smAndDown"
+    fixed
+    :value="value"
+    color="primary"
+    grow
+  >
+    <v-btn>
+      <span>Home</span>
+      <v-icon>mdi-home-outline</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Discounts</span>
+      <v-icon>mdi-tag-text-outline</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Others</span>
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 0,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
