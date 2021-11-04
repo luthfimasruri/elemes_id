d3a69fd7d14951d82a047724a8c52fa347f4c8b6
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-11-04 20:50:22 +0700

    chore(recipe): :sparkles: add image background

--- components/Recipes.vue
+++ components/Recipes.vue
@@ -9,24 +9,28 @@
         <v-col :key="index" cols="12" md="3" sm="4">
           <nuxt-link to="/" class="text-decoration-none">
             <v-hover v-slot="{ hover }">
-              <div
-                :class="`bg-card${Math.floor(Math.random() * 5)} ${
-                  hover ? 'elevation-6' : ''
-                }`"
-                class="rounded-xl px-4 py-6"
+              <v-img
+                src="/images/bg-hero.png"
+                :class="{ 'elevation-3': hover }"
+                class="rounded-xl elevation-0'"
               >
-                <v-img
-                  width="100"
-                  height="100"
-                  class="rounded-lg"
-                  :src="`/images/recipes/${item.image}`"
-                ></v-img>
-                <h3 class="mt-3">{{ item.title }}</h3>
-                <div class="grey--text text--darken-1 mb-1">
-                  {{ item.category }}
+                <div
+                  :class="`bg-card${Math.floor(Math.random() * 5)}`"
+                  class="rounded-xl px-4 py-6"
+                >
+                  <v-img
+                    width="100"
+                    height="100"
+                    class="rounded-lg"
+                    :src="`/images/recipes/${item.image}`"
+                  ></v-img>
+                  <h3 class="mt-3">{{ item.title }}</h3>
+                  <div class="grey--text text--darken-1 mb-1">
+                    {{ item.category }}
+                  </div>
+                  <Rating :value="item.rating" />
                 </div>
-                <Rating :value="item.rating" />
-              </div>
+              </v-img>
             </v-hover>
           </nuxt-link>
         </v-col>
