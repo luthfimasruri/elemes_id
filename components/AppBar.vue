97550be65763c5271e1e4edb41d079eaf7dc0770
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-11-04 12:27:04 +0700

    refactor: :recycle: refactor navbar

---
+++ components/AppBar.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app-bar
+    v-scroll="onScroll"
+    :class="{ transparent: scrollTop }"
+    :fixed="$vuetify.breakpoint.mdAndUp"
+    :absolute="$vuetify.breakpoint.smAndDown"
+    elevate-on-scroll
+    elevation="3"
+    color="white"
+  >
+    <v-container class="d-flex align-center">
+      <nuxt-link to="/">
+        <v-img width="150" contain src="/logo.svg" alt="Logo Elemes" />
+      </nuxt-link>
+      <v-spacer />
+      <div class="d-none d-md-flex">
+        <template v-for="(item, index) in menu">
+          <v-btn
+            v-if="!item.badge"
+            :key="index"
+            text
+            rounded
+            color="grey darken-2"
+            class="mr-2"
+            >{{ item.title }}</v-btn
+          >
+          <v-badge
+            v-else
+            :key="index"
+            :content="item.badge"
+            offset-x="34"
+            color="red"
+            overlap
+            right
+          >
+            <v-btn text rounded color="grey darken-2" class="mr-2">{{
+              item.title
+            }}</v-btn>
+          </v-badge>
+        </template>
+      </div>
+      <v-spacer />
+      <v-btn text rounded color="grey darken-2" class="mr-2 font-weight-medium"
+        >Login</v-btn
+      >
+      <v-btn depressed color="primary" dark rounded>Register</v-btn>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollTop: true,
+      menu: [
+        { title: 'Home', path: '' },
+        { title: 'About', path: '' },
+        { title: 'Promotion', path: '', badge: 'HOT' },
+        { title: 'Blog', path: '' },
+        { title: 'Contact Us', path: '' },
+      ],
+    }
+  },
+  methods: {
+    onScroll(e) {
+      const body = e.target.body
+      let document = e.target.documentElement
+      document = document.clientHeight ? document : body
+      if (document.scrollTop === 0) {
+        this.scrollTop = true
+      } else {
+        this.scrollTop = false
+      }
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,16 @@
 <template>
-  <v-app v-scroll="onScroll" dark>
-    <v-app-bar :class="{ transparent: scrollTop }" fixed elevate-on-scroll color="white">
-      <Navbar />
-    </v-app-bar>
+  <v-app dark>
+    <AppBar />
     <v-main>
       <Nuxt />
     </v-main>
-    <v-footer color="transparent">
-      <Footer />
-    </v-footer>
+    <Footer />
+    <BottomNav />
   </v-app>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      scrollTop: true,
-    }
-  },
-  methods: {
-    onScroll(e) {
-      const body = e.target.body
-      let document = e.target.documentElement
-      document = document.clientHeight ? document : body
-      if (document.scrollTop === 0) {
-        this.scrollTop = true
-      } else {
-        this.scrollTop = false
-      }
-    },
-  },
+  
 }
 </script>
