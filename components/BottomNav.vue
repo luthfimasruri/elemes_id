f13d05773478facee72e10bf5731ea611b75d587
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-11-04 19:45:19 +0700

    feat(app): :sparkles: add bottom navigation

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -6,20 +6,33 @@
     color="primary"
     grow
   >
-    <v-btn>
+    <v-btn to="/">
       <span>Home</span>
       <v-icon>mdi-home-outline</v-icon>
     </v-btn>
 
-    <v-btn>
-      <span>Discounts</span>
+    <v-btn to="/promotion">
+      <span>Promotion</span>
       <v-icon>mdi-tag-text-outline</v-icon>
     </v-btn>
 
-    <v-btn>
-      <span>Others</span>
-      <v-icon>mdi-menu</v-icon>
-    </v-btn>
+    <v-menu top offset-y rounded="xl">
+      <template #activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">
+          <span>Others</span>
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(item, index) in menuOthers" :key="index" :to="item.path">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-bottom-navigation>
 </template>
 
@@ -28,11 +41,15 @@ export default {
   data() {
     return {
       value: 0,
+      menuOthers: [
+        { title: 'Blog', path: '/blog', icon: 'mdi-newspaper-variant-outline' },
+        { title: 'About Us', path: '/about-us', icon: 'mdi-information-outline' },
+        { title: 'Contact Us', path: '/contact-us', icon: 'mdi-account-box-outline' },
+      ],
     }
-  }
+  },
 }
 </script>
 
 <style>
-
 </style>
